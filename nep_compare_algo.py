0481cf62329305c62bf720cbb0d5276b344d4148
Suheel Yousuf Wani | Data Scientist | Bioinformatician | Software Developer <35189084+suheelyousuf@users.noreply.github.com>
2022-12-01 14:08:35 +0300

    Update nep_compare_algo.py

--- nep_compare_algo.py
+++ nep_compare_algo.py
@@ -1,6 +1,6 @@
 import neptune.new as neptune
 
-run = neptune.init_run(project="corp_space/fraud_detection")
+run = neptune.init_run(project="genome/lnc_detection")
 run["parameters"] = {"lr": 0.001, "optim": "Adam"} # parameters
 run["f1_score"] = 0.66 # metrics
 run["roc_curve"].upload("roc_curve.png") # charts
